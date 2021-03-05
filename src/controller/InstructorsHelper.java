00ce0a8ecb00b36105bb2db34f0dac376037ffa5
Tom Sorteberg <66091315+tsorteberg@users.noreply.github.com>
2021-03-04 23:40:36 -0600

    Added InstructorsHelper.java

---
+++ src/controller/InstructorsHelper.java
@@ -0,0 +1,18 @@
+package controller;
+
+/**
+ * @author Tom Sorteberg - tsorteberg
+ * @autor Levi Olson - lolson17
+ * CIS175 - Spring 2021
+ * Mar 4, 2021
+ */
+public class InstructorsHelper {
+
+	/**
+	 * 
+	 */
+	public InstructorsHelper() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
