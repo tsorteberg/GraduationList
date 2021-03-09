c0cd0f048d95c5eb93128bce22fc4a8c672730d9
Tom Sorteberg <66091315+tsorteberg@users.noreply.github.com>
2021-03-09 01:42:01 -0600

    Updated Instructors.java.

--- src/model/Instructors.java
+++ src/model/Instructors.java
@@ -39,5 +39,72 @@ public class Instructors {
 		super();
 	}
 	
-	// To-do
+	/**
+	 * Primary constructor.
+	 * @param id: Required integer.
+	 * @param firstName: Required string.
+	 * @param lastName: Required string.
+	 */
+	public Instructors(int id, String firstName, String lastName) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+	
+	/**
+	 * Get method for id instance.
+	 * @return: integer.
+	 */
+	public int getId() {
+		return id;
+	}
+	
+	/**
+	 * Get method for firstName instance.
+	 * @return: string.
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	/**
+	 * Get method for lastName instance.
+	 * @return: string.
+	 */
+	public String getLastName() {
+		return lastName;
+	}
+	
+	/**
+	 * Set method for id instance.
+	 * @param id: Required integer.
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	/**
+	 * Set method for firstName instance.
+	 * @param id: Required string.
+	 */
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	/**
+	 * Set method for lastName instance.
+	 * @param id: Required string.
+	 */
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	/**
+	 * Default toString() method.
+	 */
+	@Override
+	public String toString() {
+		return "Instructors [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + "]";
+	}
 }
