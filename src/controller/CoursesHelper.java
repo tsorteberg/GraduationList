1551186ee5670f248636a476af20e6a8f8bd5351
Tom Sorteberg <66091315+tsorteberg@users.noreply.github.com>
2021-03-09 03:06:46 -0600

    Updated various files.
    I updated the CoursesHelper.java, LocalDateAttributeConverter.java, ViewAllCoursesServlet.java, graduation-list.jsp, and added index.html.
    
    Assuming the database is pre-populated, the app can now list database items.

--- src/controller/CoursesHelper.java
+++ src/controller/CoursesHelper.java
@@ -1,5 +1,11 @@
 package controller;
 
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+import model.Courses;
 /**
  * @author Tom Sorteberg - tsorteberg
  * @autor Levi Olson - lolson17
@@ -9,16 +15,53 @@ package controller;
 //Mirror ListItemHelper.java from 'ShoppingList Online with JPA Servlets JSP 2020.pdf'
 public class CoursesHelper {
 
+	// Instance variable declaration and initialization.
+	static EntityManagerFactory emfactory = Persistence.createEntityManagerFactory("GraduationList");
+
 	/**
-	 * 
-	 */
+	 * Default constructor.
+	*/
 	public CoursesHelper() {
-		// TODO Auto-generated constructor stub
+	// TODO Auto-generated constructor stub
 	}
-	
-	// Insert method.
-	
-	// Show all method.
-	
-	// Delete method.
+		
+	/**
+	* Method that accepts a course to add.
+	* @param courses: Required listItem object type.
+	*/
+	public void insertItem(Courses courses) {
+		EntityManager em = emfactory.createEntityManager();
+		em.getTransaction().begin();
+		em.persist(courses);
+		em.getTransaction().commit();
+		em.close();
+		}
+		
+	/**
+	* Method that returns Courses object of all objects in the database.
+	* @return: List<Courses> object.
+	*/
+	@SuppressWarnings("unchecked")
+	public List<Courses> showAllItems() {
+		EntityManager em = emfactory.createEntityManager();
+		List<Courses> allItems = em.createQuery("SELECT i FROM Courses i").getResultList();
+		return allItems;
+	}
+		
+	/**
+	* Method that deletes the first Courses object from database.
+	* @param toDelete: Required Courses object.
+	*/
+		public void deleteItem(Courses toDelete) {
+			EntityManager em = emfactory.createEntityManager();
+			em.getTransaction().begin();
+			TypedQuery<Courses> typedQuery = em.createQuery("select ci from Courses ci where ci.course_id = :selectedId", Courses.class);
+			typedQuery.setParameter("selectedId", toDelete.getId());
+			typedQuery.setMaxResults(1);
+			Courses result = typedQuery.getSingleResult();
+			em.remove(result);
+			em.getTransaction().commit();
+			em.close();
+		}
+
 }
--- src/controller/ViewAllCoursesServlet.java
+++ src/controller/ViewAllCoursesServlet.java
@@ -27,22 +27,35 @@ public class ViewAllCoursesServlet extends HttpServlet {
      */
     public ViewAllCoursesServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		// Local variable declaration and initialization.
+		String path = "/graduation-list.jsp";
+				
+		// Local object declaration and instantiation.
+		CoursesHelper dao = new CoursesHelper();
+				
+		// Method call to set request parameter as a list generated from a context object.
+		request.setAttribute("allItems", dao.showAllItems());
+				
+		// Selection logic to provide exception handling.
+		// If database table is empty, redirect to page to add new items.
+		if (dao.showAllItems().isEmpty()) {
+			path="/index.html";
+		}
+				
+		// Method call to redirect to index.html using request dispatcher.
+		getServletContext().getRequestDispatcher(path).forward(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
